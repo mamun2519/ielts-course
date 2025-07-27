cf679d8bdf8d0e7134ef93cef541affc8b95f54a
mamun2519 <juborajislam46@gmail.com>
2025-07-27 16:16:57 +0600

    Feat: added to navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
